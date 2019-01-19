70d4277309b58531a927572f3d10ee0cd075e854
viniciuscosta <viniciusccosta95@gmail.com>
2019-01-19 01:40:11 -0200

    Box class added

--- sudoku/sketch.js
+++ sudoku/sketch.js
@@ -2,38 +2,22 @@ const cols = 9;
 const rows = 9;
 let h;
 let w;
-let cells
 let curCell;
-let data;
+let boxes;
 
-function preload() {
-  data = loadJSON('maps.json'); // TODO: Is this the best way?
-	// How to generate a random map ?
-}
 function setup() {
   cellHeight = (windowHeight / rows) * 0.8;
-  cellWidth = cellHeight; //( windowWidth  / cols ) * 0.8; // make it square!
+  cellWidth  = cellHeight; //( windowWidth  / cols ) * 0.8; // make it square!
   createCanvas(cellWidth * cols + 1, cellHeight * rows + 1);
 
-  loadMap("easy"); // TODO: User choose which difficult
+	generateGrid();
+	noLoop(); // We don't have to draw if the user didn't interect
 }
 function draw() {
-
-  for (let cel of cells) {
-    cel.update();
-    cel.show();
-  }
-
-  // Borders:
-  push();
-  fill(255, 0);
-  strokeWeight(4);
-  for (let row = 0; row < rows; row += 3) {
-    for (let col = 0; col < cols; col += 3) {
-      rect(row * cellWidth + 1, col * cellHeight + 1, 3 * cellWidth - 1, 3 * cellHeight - 1);
-    }
-  }
-  pop();
+	for(let box of boxes){
+		box.update();
+		box.show();
+	}
 }
 
 function mousePressed() {
@@ -43,53 +27,84 @@ function mousePressed() {
     insideCanvas = false;
   }
 
-  let col = int(mouseX / cellWidth);
-  let row = int(mouseY / cellHeight);
+  let col  	   = int(mouseX /  cellWidth);
+  let row  	   = int(mouseY / cellHeight);
 
-  for (let cel of cells) {
-    if (insideCanvas && cel.col == col && cel.row == row) {
-      curCell = cel;
+  for (let box of boxes) {
+		for(var i = 0; i < 3; i++){
+			for(var j = 0; j < 3; j++){
+				let cel = box.cells[i][j];
 
-      if (mouseButton == LEFT) {
-        cel.writing = !cel.writing;
-        cel.commenting = false;
-      } else if (mouseButton == RIGHT) {
-        cel.writing = false;
-        cel.commenting = !cel.commenting;
-      }
-    } else {
-      cel.writing = false;
-      cel.commenting = false;
-    }
+				if (insideCanvas && cel.col == col && cel.row == row) {
+		      curCell = cel;
+
+		      if (mouseButton == LEFT) {
+		        cel.writing = !cel.writing;
+		        cel.commenting = false;
+		      } else if (mouseButton == RIGHT) {
+		        cel.writing = false;
+		        cel.commenting = !cel.commenting;
+		      }
+		    } else {
+		      cel.writing = false;
+		      cel.commenting = false;
+		    }
+			}
+		}
   }
 
+	redraw();
   return false;
 }
 function keyPressed() {
+	// TODO: Use the keyboard to change "selected" cell
+
   if (curCell) {
     if (key >= 1 && key <= 9) {
       curCell.change(key.toString());
-    } else if (keyCode == 8) {
-      curCell.clear();
-    }
-  }
+    } else{
+				switch(keyCode){
+					case 8:
+						curCell.clear(); 									// Backspace/Delete key
+						break;
+					case LEFT_ARROW:
+						break;
+					case RIGHT_ARROW:
+						break;
+					case UP_ARROW:
+						break;
+					case DOWN_ARROW:
+						break;
+    	}
+  	}
+	}
 
+	redraw();
   return false;
 }
 
-function loadMap(difficult) {
-  cells = [];
-  let maps = data[difficult];
-  let map = maps["1"]; // TODO: Choose a random map from this difficult
+function generateGrid(){
+	// Boxes:
+	boxes = [];
+	for(let i = 0; i < 9; i++) boxes.push( new Box(i) ); 	// [Box0, Box1, Box2, Box3,..., Box8]
 
-  for (row = 0; row < rows; row++) {
+	// Cells:
+	for (row = 0; row < rows; row++) {
     for (col = 0; col < cols; col++) {
-      let key = [col, row].toString();
-      cells.push(new Cell(col, row, map[key]["fixed"], map[key]["value"]));
+			let cell = new Cell(col, row, false, 0)
+
+			let boxrow   = int(row / 3);
+			let boxcol   = int(col / 3);
+			let indexBox = boxrow * 3 + boxcol; 							// | 0 -> 1 -> 2 | 3 -> 4 -> 5 | 6 -> 7 -> 8 |
+
+			boxes[indexBox].addCell(cell);
     }
   }
 
+	 // Values:
+	 boxes[0].fillRandomly();
+	 boxes[4].fillRandomly();
+	 boxes[8].fillRandomly();
 }
 
-// TODO: Cells should be just an 2D matrix? Why POO? kkkkkk
-// TODO: Class "BIG AREA" ?
+// TODO: Cells should be just an 2D matrix (it would make it alot easier HAHA)
