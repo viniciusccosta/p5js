c32db0d2df27ddb378cfdbcfded4a54b85ab8924
viniciuscosta <viniciusccosta95@gmail.com>
2019-01-25 01:11:27 -0200

    Matter.js test and lerp function.

--- snakevsblock/Ball.js
+++ snakevsblock/Ball.js
@@ -1,42 +1,34 @@
 class Ball{
   constructor(x,y){
-    this.pos  = createVector(x,y);
-    this.r    = 10;
-    this.speed =10;
-    this.velocity = createVector(0,-universalSpeed);
+    this.pos      = createVector(x,y);
+    this.r        = 10;
+    this.speed    = (2*this.r)*0.1;
+    this.velocity = createVector(0,0); // the ball can't move up/down, because there's a limit!
 
     this.tail = [];
-    for(let i = 1; i <= 14; i++){
-      this.tail.push( createVector(this.pos.x, this.pos.y + 2*i*this.r) ); // pos.y = 10 e r = 3 --> 10 + 2*1*3 = 16--> 10 + 2*2*3 = 22
-      // or: this.tail.push ( createVector(this.pos.x, this.tail[-1] + 2*this.r) )
+    for(let i = 3; i >= 1; i--){
+      this.tail.push( createVector(this.pos.x, this.pos.y + 2*i*this.r) );
     }
+    this.tail.push(this.pos);
 
   }
 
   update(){
     // TODO: TAIL IS HORRIBLE! HAHA!
     // TODO: Move with mouse/touchscreen, not keyboard
+    // TODO: It's not only "pop()", we have to bring all the tail up
 
-    // TAIL:
-    if(this.tail.length > 0){
-      for(let i = this.tail.length - 1; i > 0; i--){
-        this.tail[i].y += this.velocity.y;
-        this.tail[i].x  = this.tail[i-1].x;
-      }
-      this.tail[0].x  = this.pos.x;
-      this.tail[0].y += this.velocity.y;
+    let lastIndex = this.tail.length - 1;
+
+    // MOVE:
+    for(let i = 0; i < lastIndex; i++){
+      this.tail[i].x = lerp(this.tail[i].x, this.tail[i+1].x, 0.15);
     }
+    this.tail[lastIndex].add(this.velocity);
 
-    // MOVEMENT:
-    this.pos.add(this.velocity);
-
-    // LIMITS:
-    if(this.pos.x < this.r) {
-      this.pos.x = this.r;
-      this.velocity.x = 0;
-    }
-    if(this.pos.x > width - this.r){
-      this.pos.x = width - this.r;
+    // EDGES/BORDERS:
+    this.tail[lastIndex].x = constrain(this.tail[lastIndex].x, 0 + this.r, width - this.r);
+    if(this.tail[lastIndex].x <= this.r || this.tail[lastIndex].x >= width - this.r) {
       this.velocity.x = 0;
     }
 
@@ -44,22 +36,25 @@ class Ball{
 
   show(){
     fill(255,255,0);
-    ellipse(this.pos.x,this.pos.y,this.r*2);
+    let cnt = this.tail.length;
 
     for(let t of this.tail){
-      fill(255,127,0);
+      fill(255/cnt, 255/cnt,0);
+      cnt--;
       ellipse(t.x,t.y,this.r*2);
     }
   }
 
-  move(k, kc){
+  move(kc){
     switch(kc){
       case RIGHT_ARROW:
-        this.velocity.x += this.velocity.x >= this.speed  ? 0 : this.speed;
+        this.velocity.x += this.speed;
         break;
       case LEFT_ARROW:
-        this.velocity.x += this.velocity.x <= -this.speed  ? 0 : -this.speed;
+        this.velocity.x -= this.speed;
         break;
     }
+
+    this.velocity.x = constrain(this.velocity.x, -this.speed, this.speed);
   }
 }
--- snakevsblock/sketch.js
+++ snakevsblock/sketch.js
@@ -1,69 +1,151 @@
 const rows = 4;
 const cols = 5;
 
-let ball; 							// Player
-let blocks; 						// List of blocks
-let distMin; 						// Minimal distance between two rows
-let blockW; 						// Block Width
+let ball; // Player
+let blocks; // List of blocks
+let distMin; // Minimal distance between two rows
+let blockW; // Block Width
 let universalSpeed = 2; // Used to reduce speed when player hits a block
+
+// MATTER JS:
+let engine;
+let tail = [];
+let cnstrs = [];
+let vel;
+let useMatter = true;
 
 function setup() {
   createCanvas(350, 620);
   rectMode(CENTER);
+  //frameRate(10);
+
+  // MATTER.JS:
+  if (useMatter) {
+    // ==================================================================================================
+    // WORLD:
+    engine = Matter.Engine.create();
+
+    // SNAKE:
+    vel = createVector(0, 0);
+    let r = 8;
+    let d = 2 * r;
+    for (let i = 0; i < 10; i++) {
+      tail.push(Matter.Bodies.circle(width / 2 + random(0, 0), i * d + r + width / 2, r));
+    }
+    for (let t of tail) Matter.World.add(engine.world, t);
+
+    // CONSTRAINTS:
+    for (let i = 0; i < tail.length - 1; i++) {
+      cnstrs.push(Matter.Constraint.create({
+        bodyA: tail[i],
+        bodyB: tail[i + 1],
+        length: d
+      }));
+
+      tail[i + 1].frictionAir = 0.1;
+    }
+    for (let c of cnstrs) Matter.World.add(engine.world, c);
+
+    // GRAVITY:
+    engine.world.gravity.scale = 0.001; // default: 0.001
+    tail[0].ignoreGravity = true;
+    Matter.Events.on(engine, 'beforeUpdate', function() {
+      var gravity = engine.world.gravity;
+
+      Matter.Body.applyForce(tail[0], {
+        x: 0,
+        y: 0
+      }, {
+        x: -gravity.x * gravity.scale * tail[0].mass * tail.length,
+        y: -gravity.y * gravity.scale * tail[0].mass * tail.length
+      });
+
+    });
+    // ==================================================================================================
+  }
 
   // Player:
   ball = new Ball(width * 0.5, height * 0.5);
 
   // Blocks:
-	blocks  = [];
-	blockW 	= width/cols;
-	distMin = blockW * 4;
+  blocks = [];
+  blockW = width / cols;
+  distMin = blockW * 4;
 
-	for(let row = 0; row < rows; row++){
-		blocks[row] = [];
-		for(let col = 0; col < cols; col++){
-			blocks[row][col] = new Block( ( col * blockW)  + blockW/2, (-row * distMin) + blockW/2, blockW, blockW, row );
-		}
-	}
+  for (let row = 0; row < rows; row++) {
+    blocks[row] = [];
+    for (let col = 0; col < cols; col++) {
+      blocks[row][col] = new Block((col * blockW) + blockW / 2, (-row * distMin) + blockW / 2, blockW, blockW, row);
+    }
+  }
+
 }
 
 function draw() {
   background(200);
 
-	translate(0, height/2 - ball.pos.y);
-
+  // ========================================================================================
   // Blocks:
   for (let row = 0; row < rows; row++) {
-		let rnd = random(0, 0);
+    let rnd = random(0, 0);
 
     for (let col = 0; col < cols; col++) {
       let block = blocks[row][col];
 
       if (block != null) {
         block.update();
-				block.show();
+        block.show();
 
-				if(block.offscreen){
-					blocks[row][col] = new Block(
-						block.pos.x,
-						0 /*block.pos.y - (rows*distMin + blockW/2)*/,
-						blockW,
-						blockW,
-						row); // TODO: Generate semi-randomly
-				}
+        if (block.offscreen) {
+          blocks[row][col] = new Block(
+            block.pos.x,
+            0 /*block.pos.y - (rows*distMin + blockW/2)*/ ,
+            blockW,
+            blockW,
+            row); // TODO: Generate semi-randomly
+        }
       }
     }
   }
 
+  // ========================================================================================
+  // MATTER.JS:
+  if (useMatter) {
+    fill(0, 255, 255);
+    for (let teste of tail) ellipse(teste.position.x, teste.position.y, 2 * teste.circleRadius);
+
+    fill(255, 0, 0);
+    ellipse(tail[0].position.x, tail[0].position.y, 2 * tail[0].circleRadius);
+    Matter.Body.setVelocity(tail[0], vel);
+
+    Matter.Engine.update(engine);
+  }
+
+  // ========================================================================================
 
   // Player:
-	ball.move();
-	ball.update();
+  ball.move();
+  ball.update();
   ball.show();
 }
 
-function keyPressed(){
-	ball.move(key, keyCode);
+function keyPressed() {
+  ball.move(keyCode);
+
+  // MATTER.JS
+  if (useMatter) {
+    switch (keyCode) {
+      case LEFT_ARROW:
+        vel.x -= 5;
+        vel.x = constrain(vel.x, -5, 5);
+        break;
+      case RIGHT_ARROW:
+        vel.x += 5;
+        vel.x = constrain(vel.x, -5, 5);
+        break;
+    }
+  }
+
 }
 
 // TODO: Collision
